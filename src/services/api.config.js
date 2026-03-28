ffe145b325c5c2db35e3d263e71710eaf357febb
Amulya Kumar Padhan <amulyakumarpadhan7@gmail.com>
2026-03-28 12:28:02 +0530

    Added code to use new exim ai backend including voice also.

--- src/services/api.config.js
+++ src/services/api.config.js
@@ -1,9 +1,9 @@
 const API_CONFIG = {
     // API server base for chat and auth
-    API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://finance.devapi.zipaworld.com",
+    API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://eximmcpbackend.devapi.zipaworld.com",
 
     // WebSocket Service for LiveKit
-    SOCKET_IO_URL: import.meta.env.VITE_SOCKET_IO_URL || "https://eximgptlivekitnew.devapi.zipaworld.com"
+    SOCKET_IO_URL: import.meta.env.VITE_SOCKET_IO_URL || "https://eximmcpbackend.devapi.zipaworld.com"
 };
 
 export default API_CONFIG;
